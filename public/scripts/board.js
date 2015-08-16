551889155c67ee197cacadae84097088f18ef2e2
chaosed0 <chaosed0@gmail.com>
2015-08-16 15:48:50 -0400

    Refactor and add markings

--- public/scripts/board.js
+++ public/scripts/board.js
@@ -1,47 +1,40 @@
 
-define(['jquery', './Util', './Constants', './tile_data'],
-        function($, u, c, tile_data) {
-    var Board = function() {
-        var pointInCell = function(point, cell) {
-            /* We're assuming two properties of the cells:
-             *  1. They're ordered clockwise from the top-left
-             *  2. The top and bottom lines are parallel (but not the left and right) */
-            if (point.y < cell[0].y || point.y > cell[2].y) {
-                return false;
-            }
+define(['jquery', './Util', './Constants', './Tile', './tile_data'],
+        function($, u, c, Tile, tile_data) {
+    var Board = function(origin) {
+        var board_origin = origin;
+        var tiles = [];
 
-            var leftp1 = cell[0];
-            var leftp2 = cell[3];
-            var rightp1 = cell[1];
-            var rightp2 = cell[2];
-
-            var leftx = (leftp2.x - leftp1.x) / (leftp2.y - leftp1.y) * (point.y - leftp2.y) + leftp2.x;
-            var rightx = (rightp2.x - rightp1.x) / (rightp2.y - rightp1.y) * (point.y - rightp2.y) + rightp2.x;
-
-            if (point.x >= leftx && point.x <= rightx) {
-                return true;
-            }
-            return false;
+        var offsetTile = function(cell, offset) {
+            return newTile;
         }
 
-        var offsetCell = function(cell, offset) {
-            var newCell = new Array(cell.length);
-            for (var i = 0; i < cell.length; i++) {
-                newCell[i] = {};
-                newCell[i].x = cell[i].x + offset.x;
-                newCell[i].y = cell[i].y + offset.y;
+        for (var i = 0; i < tile_data.origins.length; i++) {
+            var origin = tile_data.origins[i];
+            origin.x += board_origin.x;
+            origin.y += board_origin.y;
+            for (var j = 0; j < tile_data.tiles.length; j++) {
+                var beforeOffset = tile_data.tiles[j];
+                var points = new Array(beforeOffset.length);
+                for (var k = 0; k < beforeOffset.length; k++) {
+                    points[k] = {
+                        x: beforeOffset[k].x + origin.x,
+                        y: beforeOffset[k].y + origin.y
+                    };
+                }
+                var coords = {
+                    x: (j == 0 || j == 3 || j == 5 ? 0 : (j == 1 || j == 6 ? 1 : 2)),
+                    y: i,
+                    z: (j < 3 ? 0 : (j < 5 ? 1 : 2))
+                };
+                tiles.push(new Tile(points, coords));
             }
-            return newCell;
         }
 
-        this.getCellForPoint = function(point) {
-            for (var i = 0; i < tile_data.origins.length; i++) {
-                var origin = tile_data.origins[i];
-                for (var j = 0; j < tile_data.tiles.length; j++) {
-                    var tile = offsetCell(tile_data.tiles[j], origin);
-                    if (pointInCell(point, tile)) {
-                        return tile;
-                    }
+        this.getTileForPoint = function(point) {
+            for (var i = 0; i < tiles.length; i++) {
+                if (tiles[i].containsPoint(point)) {
+                    return tiles[i];
                 }
             }
             return null;
--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,16 +1,23 @@
 
-require(['jquery', './Util', './Constants', './board',
-        ], function($, u, c, Board) {
+require(['jquery', './Util', './Constants', './board', './Tile',
+        ], function($, u, c, Board, Tile) {
     var canvas = $('#overlay');
     var context = canvas[0].getContext('2d');
     var tower = $('#tictactower');
     var imgrect = tower[0].getBoundingClientRect();
-    var board = new Board();
+    var board = new Board({x: imgrect.left, y: imgrect.top});
+
+    var markSizes = [35, 60, 80];
+    var markClasses = ['left', 'mid', 'right']
+    var markSrcs = [null, 'img/X.svg', 'img/O.svg', 'img/J.svg'];
+    var allMarkClass = 'XOJ';
 
     canvas[0].width = canvas.width();
     canvas[0].height = canvas.height();
 
-    var highlightedCell = null;
+    var highlightedTile = null;
+    var lastDownTile = null;
+    var mark = Tile.Marks.X;
 
     var getMousePos = function(canvas, event) {
         var rect = canvas[0].getBoundingClientRect();
@@ -20,27 +27,82 @@ require(['jquery', './Util', './Constants', './board',
         };
     };
 
+    var putDownMark = function(tile, mark) {
+        var coords = tile.getCoords();
+        var markSize = markSizes[coords.z];
+        var markClass = markClasses[coords.x];
+        var markSrc = markSrcs[mark];
+
+        var tileCenter = {x: 0, y: 0};
+        var tilePoints = tile.getPoints();
+        for (var i = 0; i < tilePoints.length; i++) {
+            tileCenter.x += tilePoints[i].x;
+            tileCenter.y += tilePoints[i].y;
+        }
+        tileCenter.x /= tilePoints.length;
+        tileCenter.y /= tilePoints.length;
+
+        var markElem = $('<img/>');
+        markElem.addClass(allMarkClass);
+        markElem.addClass(markClass);
+        markElem.attr('width', markSize);
+        markElem.attr('height', markSize);
+        markElem.attr('src', markSrc);
+
+        $('body').append(markElem);
+        var bounds = markElem[0].getBoundingClientRect();
+        markElem.css('left', Math.floor(tileCenter.x - markSize / 2));
+        markElem.css('top', Math.floor(tileCenter.y - markSize / 2));
+
+        tile.setMark(mark);
+    };
+
     canvas.mousemove(function(event) {
-        var cell = board.getCellForPoint(getMousePos(canvas, event));
-        if ((cell == null && highlightedCell != null) || cell != highlightedCell) {
+        var tile = board.getTileForPoint(getMousePos(canvas, event));
+        if ((tile == null && highlightedTile != null) || tile != highlightedTile) {
             context.clearRect(0, 0, canvas[0].width, canvas[0].height);
         }
-        
-        if (cell != null && cell != highlightedCell) {
+
+        if (tile != null && tile != highlightedTile) {
+            var points = tile.getPoints();
             context.beginPath();
             context.fillStyle = 'rgba(125,125,125,0.5)';
             context.lineWidth = 3;
-            for (var i = 0; i < cell.length; i++) {
+            for (var i = 0; i < points.length; i++) {
                 if (i == 0) {
-                    context.moveTo(cell[i].x, cell[i].y);
+                    context.moveTo(points[i].x, points[i].y);
                 } else {
-                    context.lineTo(cell[i].x, cell[i].y);
+                    context.lineTo(points[i].x, points[i].y);
                 }
             }
             context.closePath();
             context.fill();
             context.stroke();
         }
-        highlightedCell = cell;
+        highlightedTile = tile;
+    });
+
+    canvas.mousedown(function(event) {
+        var tile = board.getTileForPoint(getMousePos(canvas, event));
+        lastDownTile = tile;
+    });
+
+    canvas.mouseup(function(event) {
+        var tile = board.getTileForPoint(getMousePos(canvas, event));
+        if (tile == null) {
+            lastDownTile = null;
+            return;
+        }
+
+        if (lastDownTile == tile && tile.getMark() == Tile.Marks.NONE) {
+            putDownMark(tile, mark);
+        }
+
+        mark++;
+        if (mark >= Tile.Marks.LENGTH) {
+            mark = Tile.Marks.X;
+        }
+
+        lastDownTile = null;
     });
 });
